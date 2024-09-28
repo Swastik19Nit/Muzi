ba3097ad0abfc2bd46d40cb96f184533eb8b1888
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2024-09-29 03:36:12 +0530

    api endpoint 2

---
+++ app/api/streams/downvote/route.ts
@@ -0,0 +1,56 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod';
+
+
+const UpvoteSchema = z.object({
+    streamId: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+    
+    const session = await getServerSession();
+    if (!session?.user?.email) {
+        return NextResponse.json(
+            { message: "Unauthenticated" },
+            { status: 403 }
+        );
+    }
+
+    try {
+        
+        const data = UpvoteSchema.parse(await req.json());
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: session.user.email
+            }
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+        await prismaClient.upvote.deleteMany({
+            where: {
+                AND: [
+                    { userId: user.id },
+                    { streamId: data.streamId }
+                ]
+            }
+        });
+
+        return NextResponse.json(
+            { message: "Upvote removed successfully" },
+            { status: 200 }
+        );
+
+    } catch (err) {
+        return NextResponse.json(
+            { message: "Error while removing upvote", error: err instanceof z.ZodError ? err.errors : err },
+            { status: 400 }
+        );
+    }
+}
---
+++ app/api/streams/upvote/route.ts
@@ -0,0 +1,50 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod';
+
+// Define schema 
+const UpvoteSchema = z.object({
+    streamId: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+    
+    const session = await getServerSession();
+    if (!session?.user?.email) {
+        return NextResponse.json(
+            { message: "Unauthenticated" },
+            { status: 403 }
+        );
+    }
+
+    try {
+        const data = UpvoteSchema.parse(await req.json());
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: session.user.email
+            }
+        });
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+        await prismaClient.upvote.create({
+            data: {
+                userId: user.id,
+                streamId: data.streamId
+            }
+        });
+        return NextResponse.json(
+            { message: "Upvoted successfully" },
+            { status: 200 }
+        );
+    } catch (err) {
+        return NextResponse.json(
+            { message: "Error while upvoting", error: err instanceof z.ZodError ? err.errors : err },
+            { status: 400 }
+        );
+    }
+}
