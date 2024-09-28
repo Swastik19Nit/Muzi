308649698fdd55e97cbfb9eda0bf023612947e82
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2024-09-29 01:35:53 +0530

    api endpoint 1

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod"
+const YT_REGEX = new RegExp("^https:\/\/www.\.youtube\.com\/watch\/?v=[\w-]{11}$")
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -9,11 +10,28 @@ const CreateStreamSchema = z.object({
 export async function POST(req: NextRequest) {
     try {
         const data = CreateStreamSchema.parse(await req.json());
-        const isYt = data.url.includes("youtube");
-        prismaClient.stream.create({
-            userId:data.creatorId,
-            url: data.url
-        })
+        const isYt = YT_REGEX.test(data.url);
+
+        if (!isYt) {
+            return NextResponse.json({
+                message: "Invalid YouTube URL"
+            }, {
+                status: 400
+            })
+
+        }
+        const extractedId = data.url.split("?v=")[1];
+
+        // const isYt = data.url.includes("youtube");
+        await prismaClient.stream.create({
+            data:{
+                userId:data.creatorId,
+                url: data.url,
+                extractedId,
+                type:"YouTube"
+            }
+            
+        });
     }
     catch (e) {
         return NextResponse.json({
