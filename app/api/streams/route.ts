7892776048c3537879a503fb8c482dd0641f13da
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2024-09-30 03:05:13 +0530

    fetching current stream

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,9 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod"
+
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api"
 const YT_REGEX = new RegExp("^(?:(?:https?:)?\\/\\/)?(?:www\\.)?(?:m\\.)?(?:youtu(?:be)?\\.com\\/(?:v\\/|embed\\/|watch(?:\\/|\\?v=))|youtu\\.be\\/)((?:\\w|-){11})(?:\\S+)?$")
 
 
@@ -23,19 +26,30 @@ export async function POST(req: NextRequest) {
 
         }
         const extractedId = data.url.split("?v=")[1];
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        // console.log(res.title);
+        // console.log(JSON.stringify(res.thumbnail.thumbnails));
 
         // const isYt = data.url.includes("youtube");
+        const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a: { width: number }, b: { width: number }) => a.width < b.width ? -1 : 1);
         const stream = await prismaClient.stream.create({
-            data:{
-                userId:data.creatorId,
+            data: {
+                userId: data.creatorId,
                 url: data.url,
                 extractedId,
-                type:"YouTube"
+                type: "YouTube",
+                title: res.title ?? "Can't find YouTube video",
+                smallImg: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "",
+                bigImg: thumbnails[thumbnails.length - 1].url ?? ""
+
+
+
             }
-            
+
         });
         return NextResponse.json({
-            message:"Added Stream",
+            message: "Added Stream",
             id: stream.id,
         })
     }
@@ -50,10 +64,10 @@ export async function POST(req: NextRequest) {
 
 }
 
-export async function GET(req:NextRequest){
+export async function GET(req: NextRequest) {
     const creatorId = req.nextUrl.searchParams.get("creatorId");
     const streams = await prismaClient.stream.findMany({
-        where:{
+        where: {
             userId: creatorId ?? "",
 
         }
